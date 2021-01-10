dcb37d0cde74b8505914ac8bdb436721ffaf1648
cecibrus <cecibrusquetti@hotmail.com>
2021-01-10 14:41:44 -0300

    Agrega manejo de errores para varios comandos

--- shell.py
+++ shell.py
@@ -32,73 +32,112 @@ class shellSO1(cmd.Cmd):
     #1. 	Copiar ( no puede ser una llamada a sistema a la función cp) - copiar
     def do_copiar(self,arg):
         'Necesita dos argumentos, el archivo a copiar y el destino. Ejemplo: copiar <archivo> <path>'
-        shutil.copy(*parse(arg))
+        try:
+            shutil.copy(*parse(arg))
+        except Exception:
+            print("Ocurrio algun error o el comando no se esta utilizando correctamente. Vea la ayuda con help copiar")
+        
         print('Con este comando se copia')
     #2. 	Mover - mover
     def do_mover(self,arg):
         #este si puede ser una llamada al sistema de la funcion mv 
+        'Mueve un archivo de un directorio a otro. Ejemplo: mover <origen> <destino>'
+        try:
+            comandom='mv '+ arg
+            os.system(comandom)
+        except Exception:
+            print("Ocurrio algun error o el comando no se esta utilizando correctamente. Vea la ayuda con help mover")
         print('Con este comando se mueve')
     #3. 	Renombrar - renombrar
     def do_renombrar(self,arg):
         'Necesita dos argumentos, el nombre actual y el nombre nuevo. Ejemplo> renombrar <archivo> <nombrenuevo>'
-        os.rename(*parse(arg))
+        try:
+            os.rename(*parse(arg))
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help renombrar')
         print('Con este comando se renombra')
     #4. 	Listar un directorio (no puede ser una llamada a sistema a la función ls) - listar
     def do_listar(self,arg):
         'No necesita argumentos, basta con introducir solo el comando'
-        lista=os.listdir(path='.')
-        print (lista)
+        try:
+            lista=os.listdir(path='.')
+            print (lista)
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help listar')
         print('Con este comando se lista')
     #5. 	Crear un directorio - creardir
     def do_creardir(self,arg):
         'Se necesita el nombre del directorio a ser creado. Ejemplo: creardir <nombre>'
-        path='./'+arg
-        os.mkdir(path)
+        try:
+            path='./'+arg
+            os.mkdir(path)
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help creardir')
         print('Con este comando se crea un directorio')
     #6. 	Cambiar de directorio (no puede ser una llamada a sistema a la función cd) - ir
     def do_ir(self,arg):
         'Se necesita la direccion del directorio al que se quiere cambiar'
         #si el argumento esta vacio debe llevar a home
-        os.chdir(arg)
+        try:
+            os.chdir(arg)
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help ir')
         print('Con este comando se cambia de directorio')
     #7. 	Cambiar los permisos sobre un archivo o un conjunto de archivos - permisos
     def do_permisos(self,arg):
         'Se debe pasar el nombre del archivo y el modo. Ejemplo: permisos <archivo> <777>'
-        os.chmod(*parse(arg))
+        try: 
+            os.chmod(*parse(arg))
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help permisos')
         print('Con este comando se cambian permisos')
     #8. 	Cambiar los propietarios sobre un archivo o un conjunto de archivos. - propietario
     def do_propietario(self,arg):
         #os.chown(path, uid, gid, *, dir_fd=None, follow_symlinks=True)
-        shutil.chown(*parse(arg)) #--> es de mas alto nivel que el de os
+        try:
+            shutil.chown(*parse(arg)) #--> es de mas alto nivel que el de os
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help propietario')
         print('Con este comando se cambian propietarios')
     #9. 	Cambiar la contraseña - contraseña
     def do_contrasenha(self,arg):
         'Cambia la contrasenha del usuario. Es necesario introducir solo el comando'
-        os.system('passwd')
+        try:
+            os.system('passwd')
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help contrasenha')
         print('Con este comando se cambia la contrasenha')
     #10.  Agregar usuario, y deben registrar los datos personales del mismo incluyendo su horario de trabajo y posibles lugares de conexión (ejemplo IPs o localhost). - usuario
     def do_usuario(self,arg):
         'Añade un nuevo usuario. Ejemplo: usuario <nuevoNombre>'
-        comandon='useradd'+arg
-        os.system(comandon)
+        try:
+            comandou='useradd '+arg
+            os.system(comandou)
+        except Exception:
+            print('Ocurrio un error o el comando no se esta utilizando correctamente. Vea la ayuda con help usuario')
         print('Con este comando se crea un usuario')
     #11.  El usuario debe poder levantar y apagar demonios dentro del sistema, utilizando una herramienta como service de CentOS. (no puede ser una llamada a sistema a la función service o systemctl)
     def do_demonio(self,arg):
+        'Levana o apaga demonios. Necesita como parametro la accion y el ID de proceso. Ejemplo: demonio levantar/apagar <PID>'
         #se tienen que tener mas parametros como levantar y apagar
+        print('hola demonio')
         #os.kill para apagar, se necesita el id de proceso
-        #investigar como levantar
+        #investigar como levantar --> subprocess.Popen
     #12.  Proveer la capacidad de poder ejecutar comandos del sistema, que no sean los comandos mencionados arriba.
     def do_shell(self, arg):
         'Ejecutar un comando ya existente de linux'
         print ("Ejecutando comando del shell:", arg)
         output = os.popen(arg).read()
         print (output)
+        #creo que no necesita manejo de errores ya que se hace solo
     #13.  Registrar el inicio de sesión y la salida sesión del usuario. Se puede comparar con los registros de su horario cada vez que inicia/cierra la sesión y si esta fuera del rango escribir en el archivo de log (usuario_horarios_log) un mensaje que aclare que está fuera del rango y deben agregar el lugar desde donde realizó la conexión que también puede estar fuera de sus IPs habilitado.
     def registroUsuario():
+        print("aca se registran las actividades del usuario")
         #se agrega la hora y la fecha
         #tambien se tienen que tener las IPs
     #14.  Ejecutar una transferencia por ftp o scp, se debe registrar en el log Shell_transferencias del usuario. 
     def do_transferencia(self,arg):
+        print("ftp o scp")
         #se debe revisar tambien si tiene otros parametros, como ftp o scp y de acuerdo a eso ver
 
 def parse(arg):
